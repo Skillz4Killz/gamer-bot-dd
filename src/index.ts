a0d16e026c32dc48acf071a6c5d31e29033940c0
Skillz <gankstarsleague@gmail.com>
2023-01-20 13:29:05 -0600

    fixing yarn crap idk
    Co-authored-by: Jonathan Ho <heiheiho000@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {}
+import { createRestManager } from '@discordeno/rest'
+import { DISCORD_TOKEN } from './configs'
+
+// export const DISCORD_TOKEN = ''
+
+const rest = createRestManager({
+  token: DISCORD_TOKEN,
+})
+
+rest.sendMessage('806947972004839444', { content: 'WHATEVER' })
