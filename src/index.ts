a995cfb931eb1c9530c95c78662e7f222c6c75fb
Skillz <gankstarsleague@gmail.com>
2023-01-20 14:55:12 -0600

    fix: rate limit testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createRestManager } from '@discordeno/rest'
-import { DISCORD_TOKEN } from './configs'
+import { DISCORD_TOKEN } from './configs.js'
 
 // export const DISCORD_TOKEN = ''
 
@@ -7,4 +7,13 @@ const rest = createRestManager({
   token: DISCORD_TOKEN,
 })
 
-rest.sendMessage('806947972004839444', { content: 'WHATEVER' })
+export async function testing() {
+    const test = await rest.sendMessage('806947972004839444', { content: 'Let me be ITOH!' })
+
+    for (let i = 0; i < 100; i++) {
+        await rest.sendMessage('806947972004839444', { content: 'Lit be knoen ITOH is the best(testing rate limiter)' + i + 'yui is amazing' })
+    }
+    console.log(test);
+}
+
+testing();
